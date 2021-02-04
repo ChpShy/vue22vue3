e07a00079b603c4f298ec595b210cb464413bfc2
chen <786156072@qq.com>
2021-02-04 14:48:41 +0800

    create compiler

---
+++ packages/compiler/README.md
@@ -0,0 +1,11 @@
+# `tv3-compiler`
+
+> TODO: description
+
+## Usage
+
+```
+const tv3Compiler = require('tv3-compiler');
+
+// TODO: DEMONSTRATE API
+```
--- packages/tv3/src/index.ts
+++ packages/tv3/src/index.ts
@@ -1,5 +1,7 @@
+import tv3Compiler from '@tv3/compiler';
 function tv3() {
     console.log('tv3 ing');
+    tv3Compiler({context: process.cwd()});
 }
 
 export default tv3;
